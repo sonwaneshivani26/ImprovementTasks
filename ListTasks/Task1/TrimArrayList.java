8013793db0f46d84566ca570874e799d0810dcfe
sonwaneshivani26 <sonwane_shivani@epam.com>
2025-03-27 14:48:54 +0530

    Update TrimArrayList.java

--- ListTasks/Task1/TrimArrayList.java
+++ ListTasks/Task1/TrimArrayList.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 
 public class TrimArrayList {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(10); // Initial capacity 10
+        ArrayList<Integer> list = new ArrayList<>(10); 
         list.add(1);
         list.add(2);
         list.add(3);
 
         System.out.println("Before trimming: " + list);
-        list.trimToSize();  // Trimming the capacity
+        list.trimToSize(); 
         System.out.println("After trimming: " + list);
     }
 }
